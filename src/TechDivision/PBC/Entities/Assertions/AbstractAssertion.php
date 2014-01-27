7f8b922818752da3d68f9bf3d47d3ec279460bc3
wickb <b.wick@techdivision.com>
2014-01-27 12:12:43 +0100

    added new possibility to check assertion syntax

--- src/TechDivision/PBC/Entities/Assertions/AbstractAssertion.php
+++ src/TechDivision/PBC/Entities/Assertions/AbstractAssertion.php
@@ -11,6 +11,7 @@ namespace TechDivision\PBC\Entities\Assertions;
 
 use TechDivision\PBC\Exceptions\ParserException;
 use TechDivision\PBC\Interfaces\Assertion;
+use TechDivision\PBC\Utils\PhpLint;
 
 /**
  * Class Assertion
@@ -65,31 +66,10 @@ abstract class AbstractAssertion implements Assertion
      */
     public function isValid()
     {
-        // We need our PHP parser
-        $parser = new \PHPParser_Parser(new \PHPParser_Lexer);
+        // We need our lint class
+        $lint = new PhpLint();
 
-        // Get the code wrapped in an if
-        $code = '<?php if(' . $this->getString() . '){}';
-
-        try {
-
-            // Parse it
-            $stmts = $parser->parse($code);
-
-        } catch (\PHPParser_Error $e) {
-
-            // If we got a parsing error we can assume the assertion as being invalid
-            throw new ParserException('The assertion ' . $this->getString() . ' could not be integrated correctly.');
-        }
-
-        // Assertion got parsed, check if it got parsed as the right thing
-        if (is_a($stmts[0], 'PHPParser_Node_Stmt_If')) {
-
-            return true;
-
-        } else {
-
-            return false;
-        }
+        // Wrap the code as a condition for an if clause
+        return $lint->check('if(' . $this->getString() . '){}');
     }
 }
---
+++ src/TechDivision/PBC/Utils/PhpLint.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * TechDivision\PBC\Utils\PhpLint
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ */
+namespace TechDivision\PBC\Utils;
+
+/**
+ * @package     TechDivision\PBC
+ * @subpackage  Utils
+ * @copyright   Copyright (c) 2013 <info@techdivision.com> - TechDivision GmbH
+ * @license     http://opensource.org/licenses/osl-3.0.php
+ *              Open Software License (OSL 3.0)
+ * @author      Bernhard Wick <b.wick@techdivision.com>
+ */
+class PhpLint {
+
+    /**
+     * Will remove any PHP start or end tags from the code.
+     *
+     * @param $code
+     * @return mixed
+     */
+    protected function removePhpTags($code)
+    {
+        return str_replace(array('<?php', '?>', '<?', '<?='), '', $code);
+    }
+
+    /**
+     * Will check if code is PHP syntax conform.
+     *
+     * @param $code
+     * @return boolean
+     * @throws \Exception
+     */
+    public function check($code)
+    {
+        // Save the current error reporting level and set level to 0.
+        // We would get errors shown to the use if we did not do that.
+        $level = error_reporting();
+        error_reporting(0);
+
+        try {
+
+            // Eval the passed code inside a never entered if clause.
+            // That way we can make sure to not execute any bogus code
+            $result = eval('if (false){' . $this->removePhpTags($code) . '}');
+
+            // eval does not return true if there was no error, but we want to
+            if ($result === null) {
+
+                $result = true;
+            }
+
+        } catch (\Exception $e) {
+
+            // Set the error reporting to the intended level and fail
+            error_reporting($level);
+            throw $e;
+        }
+
+        // Reset the error reporting level to the original value
+        error_reporting($level);
+
+        // Return our result
+        return $result;
+    }
+} 
