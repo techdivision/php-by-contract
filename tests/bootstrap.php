7b4514dc9ed3188928266dd1e82f4814a058734d
wickb <b.wick@techdivision.com>
2014-02-04 11:23:36 +0100

    decoupling test conf from default conf

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,8 @@
 <?php
-
+// Load PBCs bootstrapping
 require_once '${php-target.dir}/${codepool}/${namespace}/${module}/src/TechDivision/PBC/Bootstrap.php';
+
+// Load the test config file
+$config = TechDivision\PBC\Config::getInstance();
+$config->load(__DIR__ . DIRECTORY_SEPARATOR . 'TechDivision' . DIRECTORY_SEPARATOR . 'PBC' .
+    DIRECTORY_SEPARATOR . 'Tests' . DIRECTORY_SEPARATOR . 'Data' . DIRECTORY_SEPARATOR . 'tests.conf.json');
