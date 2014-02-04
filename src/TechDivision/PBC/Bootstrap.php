34abc2c7f586f1862e20c502ac69828515acee43
wickb <b.wick@techdivision.com>
2014-02-04 10:19:32 +0100

    online tests failed

--- src/TechDivision/PBC/Bootstrap.php
+++ src/TechDivision/PBC/Bootstrap.php
@@ -11,12 +11,10 @@
  *             Open Software License (OSL 3.0)
  * @link       http://www.techdivision.com/
  */
-
-// I don't know how to handle that better, forgive me (or better: explain me how!) TODO
 $vendorDir = '';
-if (is_dir(__DIR__ . "/../../../../../../vendor")) {
+if (realpath(__DIR__ . "/../../../../../../vendor")) {
 
-    $vendorDir = __DIR__ . "/../../../../../../vendor/";
+    $vendorDir = realpath(__DIR__ . "/../../../../../../vendor");
 
 } else {
 
@@ -24,8 +22,8 @@ if (is_dir(__DIR__ . "/../../../../../../vendor")) {
 }
 
 // Include the composer autoloader as a fallback
-$loader = require $vendorDir . 'autoload.php';
-$loader->add('TechDivision\\PBC\\', $vendorDir . 'techdivision/php-by-contract/src');
+$loader = require $vendorDir . DIRECTORY_SEPARATOR . 'autoload.php';
+$loader->add('TechDivision\\PBC\\', $vendorDir . DIRECTORY_SEPARATOR . 'techdivision/php-by-contract/src');
 
 // We have to register our autoLoader to put our proxies in place
 $autoLoader = new TechDivision\PBC\AutoLoader();
