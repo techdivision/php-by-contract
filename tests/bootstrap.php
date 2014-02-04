606144c64c027a72f7e95f96421667458a0a922b
wickb <b.wick@techdivision.com>
2014-02-04 17:11:13 +0100

    changed formatting

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * File bootstrapping the PHPUnit test environment
+ *
+ * PHP version 5
+ *
+ * @category   Php-by-contract
+ * @author     Bernhard Wick <b.wick@techdivision.com>
+ * @copyright  2014 TechDivision GmbH - <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php
+ *             Open Software License (OSL 3.0)
+ * @link       http://www.techdivision.com/
+ */
+
 // Load PBCs bootstrapping
 require_once '${php-target.dir}/${codepool}/${namespace}/${module}/src/TechDivision/PBC/Bootstrap.php';
 
