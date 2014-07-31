5daf3187d8353f16fed4543336daebd0879e6bf1
Tim Wagner <t.wagner@techdivision.com>
2014-07-31 09:22:07 +0200

    Fix PSR-2 incompatibilities

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,15 @@
 <?php
+
 /**
  * File bootstrapping the PHPUnit test environment
  *
  * PHP version 5
  *
- * @category   Php-by-contract
- * @author     Bernhard Wick <b.wick@techdivision.com>
- * @copyright  2014 TechDivision GmbH - <info@techdivision.com>
- * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
- * @link       http://www.techdivision.com/
+ * @category  Php-by-contract
+ * @author    Bernhard Wick <b.wick@techdivision.com>
+ * @copyright 2014 TechDivision GmbH - <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.techdivision.com/
  */
 
 use \TechDivision\PBC\Config;
